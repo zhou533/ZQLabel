8c1532b7327ed2c42173f685608c6d7b05215def
ZhouQiang <zhouqiang@gozap.com>
2013-02-05 16:29:03 +0800

    Base project

---
+++ Example/ZQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZQAppDelegate.h
+//  Example
+//
+//  Created by Chou Chris on 13-2-5.
+//  Copyright (c) 2013年 ZhouQiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Example
+//
+//  Created by Chou Chris on 13-2-5.
+//  Copyright (c) 2013年 ZhouQiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZQAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZQAppDelegate class]));
+    }
+}
